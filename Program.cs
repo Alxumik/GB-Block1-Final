9c92a0622d31998527df1842890a09d92b73eb5f
Anton Khoroshaev <vax@site.com>
2023-03-22 17:31:38 +0300

    Написана программа реализующая заданный алгоритм

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число строк данных к обработке: ");
+string N = Console.ReadLine();
+Console.WriteLine("Введите строки данных к обработке: ");
+
+int n = System.Convert.ToInt32(N);
+string[] strInput = new string[n];
+int count = 0;
+
+for (int i = 0; i < n; i++){
+    strInput[i] = Console.ReadLine();
+    if(strInput[i].Length <= 3) count++;
+}
+
+string[] strOutput = new string[count];
+int j = 0;
+
+for (int i = 0; i < n; i++) if(strInput[i].Length <= 3) strOutput[j++] = strInput[i];
+
+for (int i = 0; i < count; i++) Console.WriteLine(strOutput[i]);
